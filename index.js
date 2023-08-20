6662de6f63ec5b458c4efa4e77d9ac97b9946f6c
Gregory Salinas <gregory.salinas@gmail.com>
2023-08-19 22:22:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const waifuImage = document.querySelector('#waifus');
+const waifuButton = document.querySelector('#waifuButton');
+const waifuButtonNsfw = document.querySelector('#waifuButtonNsfw');
+
+
+const makeImages = (waifus) => {
+    for(let result of waifus) {
+        const img = document.createElement('IMG');
+        img.src.url = result.show.image.medium;
+        document.body.append(img);
+    }
+}
+
+// Safe For Work
+waifuButton.addEventListener('click', () => {
+    fetch('https://api.waifu.im/search')
+      .then(res => res.json())
+      .then(res => {
+        waifuImage.src = res.images[0].url;
+      })
+      .catch(err => console.log(err))
+  })
+
+// Not Safe For Work
+  waifuButtonNsfw.addEventListener('click', () => {
+    fetch('https://api.waifu.im/search/?is_nsfw=true')
+      .then(res => res.json())
+      .then(res => {
+        waifuImage.src = res.images[0].url;
+      })
+      .catch(err => console.log(err))
+  })
